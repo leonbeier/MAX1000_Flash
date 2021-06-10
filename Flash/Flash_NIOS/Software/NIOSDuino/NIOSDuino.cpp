71f1cc5349c6ef65b5cbf169e769aeb7821ecbca
Leon Beier <lbeier@vhdplus.com>
2021-06-10 20:29:10 +0200

    .

--- Flash/Flash_NIOS/Software/NIOSDuino/NIOSDuino.cpp
+++ Flash/Flash_NIOS/Software/NIOSDuino/NIOSDuino.cpp
@@ -2,10 +2,10 @@
 #include <Arduino.h>
 
 void setup() {
-    Serial.begin(9600); //Set Baudrate with "New Processor"
+    Serial0.begin(9600); //Set Baudrate with "New Processor"
 }
 
 void loop() {
-    Serial.println("Hello World");
+    Serial0.println("Hello World");
     delay(1000);
 }
